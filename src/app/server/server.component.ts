a557a8ba5aad83e0865389339c20662e7ab52cb2
rajji123900 <rajitha@seed04.local>
2021-07-30 14:59:00 +0530

    folders

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component ({
+ selector:"app-server",
+ templateUrl:"./server.component.html",
+ styles:[`
+ .online{
+     color:blue;
+ }`]
+})
+
+export class ServerComponent {
+ serverId:number = 10;
+ serverStatus = 'offline';
+  
+ constructor(){
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+ }
+ getServerStatus() {
+
+     return this.serverStatus;
+ }
+
+ getColor(){
+  return this.serverStatus === 'online' ? "pink" : "green "
+ }
+
+}
